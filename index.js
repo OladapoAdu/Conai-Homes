a9d2d71a5613e350fd72960f66792437f3c94e60
Destiny Oladapo Adu <aduoladapo9@gmail.com>
2025-09-21 00:27:54 +0100

    Further Preloader Tweaks.

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
- window.addEventListener("load", function(){
-    document.getElementById("preloader").style.display = "none";
-  });
+window.addEventListener("load", function(){
+    let loader = document.getElementById("preloader");
+    loader.style.opacity = "0";
+    loader.style.transition = "opacity 0.5s ease";
+    setTimeout(() => loader.style.display = "none", 500);
+});
